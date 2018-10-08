acf05f67f4187ab42f58b5ef2d3eb1eaca513cb9
abrown2 <alan.brown@disney.com>
2018-10-08 16:02:42 +0100

    add refresh button

--- src/cljs/happy_weather/views.cljs
+++ src/cljs/happy_weather/views.cljs
@@ -3,8 +3,14 @@
    [re-frame.core :as re-frame]
    [happy-weather.subs :as subs]))
 
+(defn refresh-button [app-state]
+  [:button {}
+   "Refresh"])
+
+
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]]))
+     [:h1 "Hello from " @name]
+     [refresh-button]]))
